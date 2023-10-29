7ff890c615fe443003cb3f614612a0a16cbfe5d5
unknown <soslidesong@gmail.com>
2023-10-29 16:53:29 -0500

    register route

--- server/src/db/users.js
+++ server/src/db/users.js
@@ -3,7 +3,33 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const UserModel = mongoose.model("User", UserSchema);
+
+export const createUser = (value) =>
+  // new UserModel(value).save().then((user) => user.toObject());
+  new UserModel(value).save();
+
+export const userExists = async (username, email) => {
+  try {
+    const user = await UserModel.findOne({
+      $or: [{ username: username }, { email: email }],
+    });
+    return user !== null;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getUser = async (email) => {
+  try {
+    const user = await UserModel.findOne({ email: email });
+    return user;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export default UserModel;
--- server/src/router/authentication.js
+++ server/src/router/authentication.js
@@ -1,23 +1,44 @@
 import express from "express";
-import UserModel from "../db/users.js";
+import UserModel, { createUser, userExists, getUser } from "../db/users.js";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send([{ id: "3" }, { id: "4" }]);
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
 });
 
 router.post("/register", async (req, res) => {
   try {
-    const { username, email } = req.body;
-    const user = await createUser({ username, email });
-    console.log(user);
-    res.status(200).json(user).end();
+    const { username, email, password } = req.body;
+
+    const regex_password =
+      /^(?=.*[\d]{1,})(?=.*[a-z]{1,})(?=.*[A-Z]{1,})(?=.*[<>(){}\"|;':.,~!?@#$%^=&*\\\[\]]{1,})[a-z\dA-Z<>(){}\"|;':.,~!?@#$%^=&*\\\[\]]{10,}$/;
+    const regex_email = /^[a-zA-Z\d\.\-_]+@[a-zA-Z\d-]+\.[a-zA-Z\d]{2,8}$/;
+
+    if (!regex_email.test(email)) {
+      return res.status(401).send({ msg: "email: wrong format" });
+    }
+    if (!regex_password.test(password)) {
+      return res.status(401).send({ msg: "password: wrong format" });
+    }
+
+    if (await userExists(username, email)) {
+      return res.status(401).send({ msg: "username or email already exists" });
+    }
+
+    const password_hashed = await bcrypt.hash(password, 10);
+
+    await createUser({
+      username,
+      email,
+      password: password_hashed,
+    });
+
+    res.status(200).send({ msg: "REGISTERED SUCCESSFULLY" });
   } catch (e) {
     console.log(e.message);
   }
 });
-const createUser = (value) =>
-  new UserModel(value).save().then((user) => user.toObject());
 
 export default router;
