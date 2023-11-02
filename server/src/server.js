67e540366da91f5a5d5d235acff4475b6c5b8a3e
pritika292 <pritika98@gmail.com>
2023-11-02 15:45:49 -0500

    Added support for env variables

--- server/src/server.js
+++ server/src/server.js
@@ -3,22 +3,23 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import mongoose from "mongoose";
 import AuthRoute from "./router/authentication.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 app.use(cookieParser());
+const port = process.env.PORT || 8080;
 
 app.use("/api/auth", AuthRoute);
 
-app.listen(8080, () => {
-  console.log("server started on port 8080");
+app.listen(port, () => {
+  console.log("server started on port", port);
 });
 
-const MONGO_URL =
-  "mongodb+srv://admin:admin@cluster0.ekw0gpp.mongodb.net/?retryWrites=true&w=majority";
 mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("error", (e) => console.error(e));
