a3163c961d5148e9f7f253b5365b386519571a47
Fivetr <soslidesong@gmail.com>
2023-11-16 18:32:15 -0600

    search functionality completed

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -7,6 +7,7 @@ function SearchPage() {
   const [Products, setProducts] = useState();
   const [Page, setPage] = useState(1);
   const [MaxPage, setMaxPage] = useState();
+  const [SearchInput, setSearchInput] = useState();
   useEffect(() => {
     const fetchPRoducts = async () => {
       const response = await fetch("/api/search/products");
@@ -26,26 +27,42 @@ function SearchPage() {
       setPage(1);
     } else setPage((prev) => (prev += 1));
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const filteredProducts = Products.filter((product) => {
+      return (
+        product.product_name
+          .toLowerCase()
+          .includes(SearchInput.toLowerCase()) ||
+        product.product_type
+          .toLowerCase()
+          .includes(SearchInput.toLowerCase()) ||
+        product.clean_ingreds
+          .map((item) => item.toLowerCase())
+          .includes(SearchInput.toLowerCase())
+      );
+    });
+    setProducts(filteredProducts);
+    setPage(1);
+    setMaxPage(Math.ceil(filteredProducts.length / 10));
+  };
   return (
     <>
       <Header />
       <div className="tw-p-6 tw-mt-5 tw-max-w-5xl tw-mx-auto">
-        <form>
-          <div></div>
-          <label className="tw-mb-2 tw-text-sm tw-font-medium tw-text-gray-900 tw-sr-only tw-dark:text-white">
-            Search
-          </label>
+        <form onSubmit={handleSubmit}>
           <div className="tw-relative">
             <input
               type="search"
               id="default-search"
-              className="tw-block tw-w-full tw-p-4 tw-ps-10 tw-text-sm tw-text-gray-900 tw-border tw-border-gray-300 tw-rounded-lg tw-bg-gray-50 focus:tw-ring-blue-500 focus:tw-border-blue-500 "
+              className="tw-block tw-w-full tw-p-4 tw-ps-10 tw-text-sm tw-text-gray-900 tw-border tw-border-gray-300 tw-rounded-lg tw-bg-gray-50 "
               placeholder="Search Products..."
+              onChange={(e) => setSearchInput(e.target.value)}
               required
             />
             <button
               type="submit"
-              className="tw-text-white tw-absolute tw-end-2.5 tw-bottom-2.5 tw-bg-blue-700 hover:tw-bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-4 tw-py-2"
+              className="tw-text-white tw-absolute tw-end-2.5 tw-bottom-2.5 tw-bg-blue-700 hover:tw-bg-blue-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-blue-200 tw-font-medium tw-rounded-lg tw-text-sm tw-px-4 tw-py-2"
             >
               Search
             </button>
--- server/src/db/products.js
+++ server/src/db/products.js
@@ -18,3 +18,51 @@ export const GetAllProducts = async () => {
     console.error(error);
   }
 };
+export const getProductById = async (id) => {
+  try {
+    const product = await productModel.findById(id);
+    return product;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+// async function updateCleanIngreds() {
+//   try {
+//     // Find documents where clean_ingreds field is a string containing an array
+//     const documentsToUpdate = await productModel.find({
+//       clean_ingreds: { $regex: /\['.*'\]/ },
+//     });
+
+//     // let test = documentsToUpdate[0];
+
+//     // const newArray = test.clean_ingreds[0]
+//     //   .replace(/'/g, "")
+//     //   .slice(1, -1)
+//     //   .split(", ")
+//     //   .map((item) => item.trim());
+//     // console.log(newArray);
+
+//     for (const document of documentsToUpdate) {
+//       // Convert the string to an array
+//       const newArray = document.clean_ingreds[0]
+//         .replace(/'/g, "")
+//         .slice(1, -1)
+//         .split(", ")
+//         .map((item) => item.trim());
+
+//       // Update the document with the new array
+//       document.clean_ingreds = newArray;
+//       console.log(document.clean_ingreds);
+
+//       // Save the updated document
+//       await document.save();
+//     }
+
+//     console.log("Documents updated successfully");
+//   } catch (error) {
+//     console.error("Error updating documents:", error);
+//   }
+// }
+
+// updateCleanIngreds();
