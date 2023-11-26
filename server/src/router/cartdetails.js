d760f5aa0caf488202543540b11d882a4adf3ef5
gauri-sagane <gaurisagane@gmail.com>
2023-11-25 22:20:59 -0600

    Modified add to cart functionality

--- server/src/router/cartdetails.js
+++ server/src/router/cartdetails.js
@@ -11,11 +11,20 @@ router.post("/add", async(req, res) => {
     // console.log(req.body.user.user._id)
     const user = req.body.user.user._id;
     const item = req.body.product;
-    const cart = await Cart.findOne({ user: user })
+
+    // console.log(user)
+    const cart = await Cart.findOne({ userId: user })
     .then((cart) => {
         if(cart){
-            cart.items.push(item);
-            cart.save.then(() => res.end());
+            const index = cart.items.findIndex((prod) => prod._id == item._id);
+            if(index > -1){
+                cart.quantity = cart.quantity + 1;
+            }else{
+                cart.items.push(item);
+            }
+            // console.log(index);
+            
+            cart.save().then(() => res.end());
         } else{
             Cart.create({
                 userId: user,
