fa29e1c348424eb123a197f26fa7f29f56a2c09f
gauri-sagane <gaurisagane@gmail.com>
2023-12-08 18:02:09 -0600

    Implemented Paypal Transaction processing

--- server/src/db/transactions.js
+++ server/src/db/transactions.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const transactionSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    email: { type: String, required: true },
     orderId: { type: String, required: true },
     orderData: { type: Array, required: true, default: [] },
     date: { type: Date, default: Date.now }
